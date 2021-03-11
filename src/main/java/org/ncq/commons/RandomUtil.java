2e681d97404c956699570990782f2f27cf0ab95b
niuchangqing <ncq1993@188.com>
2021-03-11 20:49:10 +0800

    添加随机工具和字符串工具

--- src/main/java/org/ncq/commons/RandomUtil.java
+++ src/main/java/org/ncq/commons/RandomUtil.java
@@ -1,12 +1,44 @@
 package org.ncq.commons;
 
+import java.awt.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 随机相关工具类
  * @author niuchangqing
  */
-public class RandomUtil {
+public final class RandomUtil {
+
+    /**
+     * 0～9数字字符串
+     */
+    public static final String NUMBER = "0123456789";
+
+    /**
+     * a~z小写字母
+     */
+    public static final String LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+
+    /**
+     * A~Z大写字母
+     */
+    public static final String UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /**
+     * 0～9,a～z,A～Z字符串
+     */
+    public static final String LOWER_UPPER_CASE_NUMBER = NUMBER + LOWER_CASE + UPPER_CASE;
+
+    /**
+     * RGB颜色范围上限
+     */
+    public static final int RGB_LIMIT = 256;
+
+    /**
+     * 私有构造方法
+     */
+    private RandomUtil(){
+    }
 
     /**
      * 获取随机对象
@@ -30,11 +62,77 @@ public class RandomUtil {
 
     /**
      * 获取随机数据,范围[min,max]。不包含max
-     * @param min
-     * @param max
-     * @return
+     * @param min       随机数最小范围
+     * @param max       随机数最大范围,不包含max。
+     * @return          返回int类型随机数字
      */
     public static int randomInt(int min, int max) {
         return getRandom().nextInt(min, max);
+    }
+
+    /**
+     * 随机获取boolean
+     * @return          返回true或者false
+     */
+    public static boolean randomBoolean() {
+        return getRandom().nextBoolean();
+    }
+
+    /**
+     * 随机获取指定长度的字符串
+     * 取样字符串 {@link org.ncq.commons.RandomUtil#LOWER_UPPER_CASE_NUMBER}
+     * @param length    随机获取字符串的长度
+     * @return          随机字符串
+     */
+    public static String randomString(int length) {
+        return randomString(length, LOWER_UPPER_CASE_NUMBER);
+    }
+
+
+    /**
+     * 随机从指定的取样字符串中获取指定长度的字符串
+     * @param length    随机获取字符串的长度
+     * @param str       取样字符串
+     * @return          随机字符串
+     */
+    public static String randomString(int length, String str){
+        if (length <= 0 || StringUtil.isEmpty(str)) {
+            return StringUtil.EMPTY;
+        }
+        StringBuilder sb = new StringBuilder();
+        int randomLength = str.length();
+        for (int i = 0; i < length; i++) {
+            int index = randomInt(randomLength);
+            sb.append(str.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 随机获取long类型随机数
+     * @param max       随机数最大值,不能小于或等于0,最大值不包含max
+     * @return          返回long类型随机数
+     */
+    public static long randomLong(long max) {
+        return randomLong(0, max);
+    }
+
+    /**
+     * 随机获取long类型随机数
+     * max不能小于等于min
+     * @param min       随机数最小值
+     * @param max       随机数最大值,最大值不包含max
+     * @return          long类型随机数
+     */
+    public static long randomLong(long min, long max) {
+        return getRandom().nextLong(min, max);
+    }
+
+    /**
+     * 随机获取一种颜色
+     * @return      随机颜色
+     */
+    public static Color randomColor() {
+        return new Color(randomInt(RGB_LIMIT), randomInt(RGB_LIMIT), randomInt(RGB_LIMIT));
     }
 }
--- src/test/java/org/ncq/commons/RandomUtilTest.java
+++ src/test/java/org/ncq/commons/RandomUtilTest.java
@@ -2,14 +2,33 @@ package org.ncq.commons;
 
 import org.junit.Test;
 
+import java.awt.*;
+
 /**
  * @author niuchangqing
  */
-public class RandomUtilTest {
+public final class RandomUtilTest {
 
     @Test
     public void randomTest(){
         int i1 = RandomUtil.randomInt(10);
         System.out.println(i1);
+    }
+
+    @Test
+    public void randomLongTest() {
+        long l = RandomUtil.randomLong(1);
+        System.out.println(l);
+    }
+
+    @Test
+    public void randomColorTest() {
+        Color color = RandomUtil.randomColor();
+    }
+
+    @Test
+    public void randomStrTest() {
+        String s = RandomUtil.randomString(1,"");
+        System.out.println(s);
     }
 }
